a23d906cc4ce0b91dfd7086a4412fcf24f95909e
Kepka Ludovic <shionn@gmail.com>
2025-08-09 01:56:38 +0200

    Premier jet acces BDD

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,11 +1,15 @@
 from flask import Flask
-# from flask_sqlalchemy import SQLAlchemy
-# import os
-
-# file_path = os.path.abspath(os.getcwd())+"/todo.db"
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import text
 
 webapp = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+file_path
-# db = SQLAlchemy(app)
+webapp.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(webapp)
 
 from webapp import routes
+
+with webapp.app_context():
+    with open('schema.sql', 'r') as f:
+        sql_script = f.read()
+    db.session.execute(text(sql_script))
+    db.session.commit()
