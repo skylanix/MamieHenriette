f7a8b262af617115032a8fe4b34239db563c3995
Kepka Ludovic <shionn@gmail.com>
2025-08-09 16:55:21 +0200

    Resoudre les probleme des 2 thread bloquant

--- run-web.py
+++ run-web.py
@@ -1,5 +1,25 @@
-from webapp import webapp
-import discordbot
+# 
+# import discordbot
+
+import multiprocessing
+import logging
+
+def start_server(): 
+	logging.info("Start Web Serveur")
+	from webapp import webapp
+	from waitress import serve
+	serve(webapp, host="0.0.0.0", port=5000)
+
+def start_discord_bot():
+	logging.info("Start Discord Bot")
+	from discordbot import bot
+	bot.begin()
 
 if __name__ == '__main__':
-	webapp.run(debug=True)
+	jobs = []
+	jobs.append(multiprocessing.Process(target=start_server))
+	jobs.append(multiprocessing.Process(target=start_discord_bot))
+
+	for job in jobs: job.start()
+	for job in jobs: job.join()
+
--- webapp/__init__.py
+++ webapp/__init__.py
@@ -2,4 +2,4 @@ from flask import Flask
 
 webapp = Flask(__name__)
 
-from webapp import commandes, index, humeurs, messages, moderation
+from webapp import commandes, configurations, index, humeurs, messages, moderation
