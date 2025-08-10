ecfc261272fa0867a7a7d558ea5ba7bd8ea60d6b
Kepka Ludovic <shionn@gmail.com>
2025-08-11 01:03:00 +0200

    Premlier essai de humble bundle, j'arrive pas à lister les canauxx dans la page de configuration

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from discordbot import bot
 
 webapp = Flask(__name__)
 
--- webapp/configurations.py
+++ webapp/configurations.py
@@ -2,10 +2,20 @@ from flask import render_template, request, redirect, url_for
 from webapp import webapp
 from database import db
 from database.models import Configuration
+from discordbot import bot
 
 @webapp.route("/configurations")
 def openConfigurations():
-	return render_template("configurations.html")
+	all = Configuration.query.all()
+	configurations = {conf.key: conf for conf in all}
+	
+	return render_template("configurations.html", configurations = configurations, channels = bot.get_all_channels())
+
+@webapp.route("/updateConfiguration", methods=['POST']) 
+def updateConfiguration():
+	
+	return redirect(url_for('openConfigurations'))
+
 
 @webapp.route('/configurations/set/<key>', methods=['POST'])
 def setConfiguration(key):
