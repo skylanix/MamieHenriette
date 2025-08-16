07f677312ce512b8ba14b1969c0990f3368f7083
skylanix <skylanix@users.noreply.github.com>
2025-08-16 19:55:25 +0200

    Enlever SIGTERM et avec SIGINT trop violent, attente corrections shionn

--- run-web.py
+++ run-web.py
@@ -1,48 +1,25 @@
-
 import locale
 import logging
-import os
-import signal
-import sys
 import threading
 from webapp import webapp
 from discordbot import bot
 
 def start_server(): 
-	logging.info("Start Web Serveur")
-	from waitress import serve
-	serve(webapp, host="0.0.0.0", port=5000)
+    logging.info("Start Web Serveur")
+    from waitress import serve
+    serve(webapp, host="0.0.0.0", port=5001)
 
 def start_discord_bot():
-	logging.info("Start Discord Bot")
-	with webapp.app_context():
-		bot.begin()
-
-def signal_handler(sig, frame):
-	logging.info("Arrêt immédiat...")
-	os._exit(0)
+    logging.info("Start Discord Bot")
+    with webapp.app_context():
+        bot.begin()
 
 if __name__ == '__main__':
-	locale.setlocale(locale.LC_TIME, 'fr_FR.UTF-8')
-	
-	signal.signal(signal.SIGINT, signal_handler)
-	signal.signal(signal.SIGTERM, signal_handler)
+    locale.setlocale(locale.LC_TIME, 'fr_CH.UTF-8')
 
-	jobs = []
-	discord_thread = threading.Thread(target=start_discord_bot)
-	server_thread = threading.Thread(target=start_server)
-	
-	discord_thread.daemon = True
-	server_thread.daemon = True
-	
-	jobs.append(discord_thread)
-	jobs.append(server_thread)
+    jobs = []
+    jobs.append(threading.Thread(target=start_discord_bot))
+    jobs.append(threading.Thread(target=start_server))
 
-	try:
-		for job in jobs: job.start()
-		for job in jobs: job.join()
-	except KeyboardInterrupt:
-		logging.info("Arrêt immédiat demandé")
-		os._exit(0)
-
-
+    for job in jobs: job.start()
+    for job in jobs: job.join()
