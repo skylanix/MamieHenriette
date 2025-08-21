dfb22ce52e890be70c78432ce5aa109797910e2b
Kepka Ludovic <shionn@gmail.com>
2025-08-21 08:03:57 +0200

    Extraction code humble bundle pour une meileur lecture

---
+++ discordbot/humblebundle.py
@@ -0,0 +1,47 @@
+import datetime
+import logging
+import json
+import requests
+
+from database import db
+from database.helpers import ConfigurationHelper
+from database.models import  GameBundle
+from discord import Client
+
+
+def _isEnable():
+	return ConfigurationHelper().getValue('humble_bundle_enable') and ConfigurationHelper().getIntValue('humble_bundle_channel') != 0
+
+def _callHexas():
+	response = requests.get("http://hexas.shionn.org/humble-bundle/json", headers={ "Content-Type": "application/json" })
+	if response.status_code == 200:
+		return response.json()
+	logging.error(f"Erreur de connexion {response.status_code}")
+	return None
+
+def _isNotAlreadyNotified(bundle):
+	return GameBundle.query.filter_by(id=bundle['id']).first() == None
+
+def _formatMessage(bundle):
+	choice = bundle['choices'][0]
+	date = datetime.datetime.fromtimestamp(bundle['endDate']/1000,datetime.UTC).strftime("%d %B %Y")
+	message = f"@here **Humble Bundle** propose un pack de jeu [{bundle['name']}]({bundle['url']}) contenant :\n"
+	for game in choice["games"]:
+		message += f"- {game}\n"
+	message += f"Pour {choice['price']}€, disponible jusqu'au {date}."
+	return message
+
+async def checkHumbleBundleAndNotify(bot: Client):
+	if _isEnable() :
+		try : 
+			bundle = _callHexas()
+			if _isNotAlreadyNotified(bundle) :
+				message = _formatMessage(bundle)
+				await bot.get_channel(ConfigurationHelper().getIntValue('humble_bundle_channel')).send(message)
+				db.session.add(GameBundle(id=bundle['id'], name=bundle['name'], json = json.dumps(bundle)))
+				db.session.commit()
+		except Exception as e:
+			logging.error(f"Erreur de connexion {e}")
+	else: 
+		logging.info('Humble bundle est désactivé')
+
