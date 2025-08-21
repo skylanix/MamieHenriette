ff2114c6323ae7ac6604f77cc8b6ee3c3a5ab7fe
Kepka Ludovic <shionn@gmail.com>
2025-08-21 09:12:47 +0200

    tentative lancement bot twitch

--- run-web.py
+++ run-web.py
@@ -1,8 +1,11 @@
 import locale
 import logging
 import threading
-from webapp import webapp
+
 from discordbot import bot
+from webapp import webapp
+# from twitchbot import twitchBot
+
 
 def start_server(): 
     logging.info("Start Web Serveur")
@@ -14,12 +17,18 @@ def start_discord_bot():
     with webapp.app_context():
         bot.begin()
 
+# def start_twitch_bot():
+#     logging.info("Start Twitch Bot")
+#     with webapp.app_context():
+#         twitchBot.begin()
+
 if __name__ == '__main__':
     locale.setlocale(locale.LC_TIME, 'fr_FR.UTF-8')
 
     jobs = []
     jobs.append(threading.Thread(target=start_discord_bot))
     jobs.append(threading.Thread(target=start_server))
+    # jobs.append(threading.Thread(target=start_twitch_bot))
 
     for job in jobs: job.start()
     for job in jobs: job.join()
