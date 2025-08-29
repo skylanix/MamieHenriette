56bc02efc9d82a570cbede0fc99659be1337aad0
Kepka Ludovic <shionn@gmail.com>
2025-08-29 17:20:44 +0200

    Essai en de prise en compte du SSL

--- webapp/configurations.py
+++ webapp/configurations.py
@@ -25,6 +25,3 @@ def updateConfiguration():
 	db.session.commit()
 	return redirect(request.referrer)
 
-@webapp.route("/configurations/twitch/help") 
-def twitchConfigurationHelp():
-	return render_template("twitch-aide.html", token_redirect_url = f'{request.url_root[:-1]}{url_for('twitchReceiveToken')}')
