16bc34d2b7894ae2fd5dfd65640940bc760a28e9
Kepka Ludovic <shionn@gmail.com>
2025-09-13 00:23:07 +0200

    Ajout de log pour la notification de live

--- twitchbot/live_alert.py
+++ twitchbot/live_alert.py
@@ -1,3 +1,5 @@
+import logging
+
 from twitchAPI.twitch import Twitch
 from twitchAPI.object.api import Stream
 
@@ -14,23 +16,30 @@ async def checkOnlineStreamer(twitch: Twitch) :
 		for alert in alerts : 
 			stream = next((s for s in streams if s.user_login == alert.login), None)
 			if stream : 
+				logging.info(f'Streamer en ligne : {alert.login}')
 				if not alert.online and alert.enable :
+					logging.info(f'N\'etait pas en ligne auparavant : {alert.login}')
 					await _notifyAlert(alert, stream)
 				alert.online = True
 			else :
+				logging.info(f'Streamer hors ligne : {alert.login}')
 				alert.online = False
 		db.session.commit()
 
 async def _notifyAlert(alert : LiveAlert, stream : Stream):
 	message : str = alert.message.format(stream)
+	logging.info(f'Message de notification : {message}')
 	bot.loop.create_task(_sendMessage(alert.notify_channel, message))
 
 async def _sendMessage(channel : int, message : str) : 
+	logging.info(f'Envoi de notification : {message}')
 	await bot.get_channel(channel).send(message)
+	logging.info(f'Notification envoyé')
 
 async def _retreiveStreams(twitch: Twitch, alerts : list[LiveAlert]) -> list[Stream] : 
 	streams : list[Stream] = []
 	async for stream in twitch.get_streams(user_login = [alert.login for alert in alerts]):
 		streams.append(stream)
+	logging.info(f'Ces streams sont en ligne : {streams}')
 	return streams
 
