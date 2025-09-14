1ad8d28bd1f3b6fac12b66a9bb5853f4c018c343
sky <comptes@favrep.ch>
2025-09-14 03:27:22 +0200

    corrige bug log qui ne s'affiche pas dans la console

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 echo "Démarrage du bot Discord..."
-exec /app/venv/bin/python run-web.py 2>&1 | rotatelogs /app/logs/app.log.%Y%m%d-%H%M%S 50M
+exec /app/venv/bin/python run-web.py 2>&1 | tee >(rotatelogs /app/logs/app.log.%Y%m%d-%H%M%S 50M)
